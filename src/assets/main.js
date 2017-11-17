fff2992be647ad46c2de7d00fe644e4b8e88143c
Vinh512 <vvvhuynh@yahoo.com>
2017-11-17 13:43:33 -0600

    created tags and appended

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2856334.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log('response', response);
+      addCourses(response.courses.completed);
+    }
+  });
+
+  function addCourses(courses) {
+    
+    var $badges = $('#badges');
+    
+    courses.forEach(function(course) {
+      // creates a div with the class course and appends it to the element with id badges
+      var $course = $('<div/>', { 'class' : 'course' }).appendTo($badges);
+      
+      // creates an h3 tag with the text contents of the course title and appends them to each of the course divs
+      $('<h3/>', { text: course.title }).appendTo($course);
+      
+      $('<img/>', { src: course.badge }).appendTo($course); 
+      
+      $('<a/>', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+           
+    })
+  }
 
 });
